ecfab2795bd4092abacf6398eb93161bceef37c2
Feng Li <feng.li@loyaltycorp.com.au>
2019-09-19 16:30:57 +1000

    feat(dev-1): upload to s3 bucket - add version to key

--- s3-sync-action/entrypoint.sh
+++ s3-sync-action/entrypoint.sh
@@ -9,11 +9,9 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-echo '####################'
-tail -n 2 yarn_release_log | head -n 1 
-echo '####################'
+VERSION=`tail -n 2 yarn_release_log | head -n 1 | cut -f 9 -d ' '`
 
-aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET} --delete --region ${AWS_REGION}
+aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET}/${VERSION} --delete --region ${AWS_REGION}
 aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths '/*'
 
 rm -rf ~/.aws
