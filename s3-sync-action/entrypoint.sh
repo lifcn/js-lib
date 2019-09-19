3389a2058c03aea5c30d5efea8139c10b5499a8d
Feng Li <feng.li@loyaltycorp.com.au>
2019-09-19 16:41:20 +1000

    feat(dev-1): create invalid distribution

--- s3-sync-action/entrypoint.sh
+++ s3-sync-action/entrypoint.sh
@@ -12,6 +12,6 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 VERSION=`tail -n 2 yarn_release_log | head -n 1 | cut -f 9 -d ' '`
 
 aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET}/${VERSION} --delete --region ${AWS_REGION}
-aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths '/*'
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/${VERSION}/*"
 
 rm -rf ~/.aws
